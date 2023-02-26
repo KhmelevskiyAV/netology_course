4812f0c770ad053432b7d777c26afe307bef6707
Alexey <khmelevskiy22@gmail.com>
2023-02-26 11:39:52 +0300

    Add task 2

---
+++ Python/General/Task_2/task_2.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a = int(input("Enter the length of the side of the square:\n"))
+    print(a * 4)
+    print(a * a)
+
+    a = int(input("Enter the length of the sides of the rectangle:\n"))
+    b = int(input("Enter the width of the sides of the rectangle:\n"))
+    print(a + b + a + b)
+    print(a * b)
